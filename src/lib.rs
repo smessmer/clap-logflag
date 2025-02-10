bdaeb5d3663df612019d76986ea52b776e6f3393
Sebastian Messmer <messmer@cryfs.org>
2025-02-09 19:58:37 -0800

    Add description

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
-//! The [clap-logflag](https://crates.io/crates/clap-logflag) library
+//! The [clap-logflag](https://crates.io/crates/clap-logflag) library adds a `--log` flag to clap based applications
+//! to allow CLI users to configure logging from the command line.
+//! It can log to stderr, files and syslog.
+//! 
