0ea32c2e8fc37a0c569d1216bad00c942ba812ae
Sebastian Messmer <messmer@cryfs.org>
2025-02-13 19:02:12 -0800

    Add documentation for the init_logging macro

--- src/fern.rs
+++ src/fern.rs
@@ -8,7 +8,50 @@ use fern::{
 
 use super::config::{LogDestination, LogDestinationConfig, LoggingConfig};
 
-/// TODO Documentation
+/// Initialize logging with the given configuration and default level.
+///
+/// # Arguments
+/// * `config` - The logging configuration to use.
+/// * `default_level` - The default log level to use if a destination was specified without a log level filter.
+///
+/// # Example
+/// ```rust
+/// use clap::Parser;
+/// use clap_logflag::LoggingConfig;
+/// use log::LevelFilter;
+///
+/// #[derive(Debug, Parser)]
+/// struct CliArgs {
+///     // Use this to add the log flags to your application
+///     #[clap(flatten)]
+///     log: clap_logflag::LogArgs,
+///     //
+///     // ... your other cli args ...
+/// }
+///
+/// fn main() {
+///     let args = CliArgs::parse();
+///
+///     // Initialize logging with the flags from clap
+///     clap_logflag::init_logging!(
+///         args.log
+///             // If no `--log` arguments are present, disable logging.
+///             // You can change this to define the default behavior,
+///             // see the "default_logging" example.
+///             .or_default(LoggingConfig::disabled()),
+///         // Any `--log` argument that doesn't define a level filter will use the
+///         // default level filter defined here, `Info` in this example.
+///         LevelFilter::Info,
+///     );
+///
+///     // Issue some log messages
+///     log::trace!("Some trace log");
+///     log::debug!("Some debug log");
+///     log::info!("Some info log");
+///     log::warn!("Some warn log");
+///     log::error!("Some error log");
+/// }
+/// ```
 #[macro_export]
 macro_rules! init_logging {
     ($config:expr, $default_level:expr $(,)?) => {{
